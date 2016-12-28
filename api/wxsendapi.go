764e36baaf6d7d70e5c2e85527caf38801420296
Ysach <farmerkael@gmail.com>
2016-12-28 18:31:15 +0800

    add channel

--- api/wxsendapi.go
+++ api/wxsendapi.go
@@ -2,15 +2,17 @@ package api
 
 import (
 	"net/http"
-	"strings"
-	"runtime"
+	//"strings"
+	//"runtime"
 	"fmt"
 	"log"
 	"encoding/json"
 	"../db/mysql"
+	"../email"
 	"../sendmsg"
 	"../wx"
 	"time"
+	"net/url"
 )
 
 type HttpRender struct {
@@ -18,7 +20,12 @@ type HttpRender struct {
 	Status int `json:"status"`
 }
 
-func Wxsend(w http.ResponseWriter, r *http.Request) {
+type DataPasser struct {
+
+	FormData chan url.Values
+}
+
+func Wxsend(w http.ResponseWriter, r *http.Request) { //不使用channel
 	r.ParseForm()
 	// fmt.Println("Method:", r.Method)
 	getType := r.Header.Get("Content-Type")
@@ -31,17 +38,37 @@ func Wxsend(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "请使用application/x-www-form-urlencoded", 404)
 			return
 		}
-		msg := strings.Join(r.Form["msg"], "")
-		runtime.GOMAXPROCS(runtime.NumCPU())
-		go wechat.PostCustomMsg("farmer", msg)
-		//erro := sendmsg.Sendmsg(msg, phone)
-		//if erro != nil {
-		//	fmt.Println(erro.Error())
-		//}
-		err := mysqldb.MysqlData(msg)
-		s := mysqldb.MysqlSel()
+		//msg := strings.Join(r.Form["msg"], "")
+		msg := r.Form.Get("msg")
+		// runtime.GOMAXPROCS(runtime.NumCPU())
+
+		err := wechat.PostCustomMsg("farmer", msg) // Send wechat messages
+		if err != nil {
+			fmt.Println("Send wechat msg error")
+		}
+		MysqlErr := mysqldb.MysqlData(msg)	// save msg in db
+		if MysqlErr != nil {
+			fmt.Println("Save db Error")
+		}
+		s, e := mysqldb.MysqlSel()	// Get phone and email string from db
+		var email sendemail.Email
+
+		email.To = e
+		email.Mailtype = "html"
+		email.Msg = "使用Golang发送邮件"
+		email.Subject = "使用Golang发送邮件"
+		fmt.Println(email)
 		fmt.Println("Start Send msg", time.Now().Unix())
-		erro := sendmsg.Sendmsg(msg, s)
+
+		emailerr := sendemail.SendMail(email) // send email
+		if emailerr != nil {
+			fmt.Println("send mail error!")
+			fmt.Println(err)
+		}else{
+			fmt.Println("send mail success!")
+		}
+
+		erro := sendmsg.Sendmsg(msg, s) // send phone messages
 		if erro != nil {
 			fmt.Println(erro.Error())
 		}
@@ -67,4 +94,77 @@ func Wxsend(w http.ResponseWriter, r *http.Request) {
 		log.Println("错误的请求！！---", r.Method, r.RequestURI)
 	}
 
+}
+
+func (p *DataPasser) MakeRender(w http.ResponseWriter, r *http.Request) {// 使用channel和并发
+
+	r.ParseForm()
+	getType := r.Header.Get("Content-Type")
+
+	switch r.Method {
+	case "POST":
+		if getType != "application/x-www-form-urlencoded" {
+			http.Error(w, "请使用application/x-www-form-urlencoded", 404)
+			return
+		}
+
+		p.FormData <- r.Form
+		var detail HttpRender
+		detail.Msg = "success"
+		detail.Status = http.StatusOK
+		body, err := json.MarshalIndent(detail, " ", " ")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Fprintf(w, string(body))
+
+	case "GET":
+		http.Error(w, "BAD Request Method", 405)
+		log.Println("错误的请求！！---", r.Method, r.RequestURI)
+
+	}
+}
+
+func (p *DataPasser) Log() { // 使用channel和并发
+	for item := range p.FormData {
+		msg := item.Get("msg")
+
+		err := wechat.PostCustomMsg("farmer", msg) // Send wechat messages
+		if err != nil {
+			fmt.Println("Send wechat msg error")
+		}
+		MysqlErr := mysqldb.MysqlData(msg)	// save msg in db
+		if MysqlErr != nil {
+			fmt.Println("Save db Error")
+		}
+		s, e := mysqldb.MysqlSel()	// Get phone and email string from db
+		var email sendemail.Email
+
+		email.To = e
+		email.Mailtype = "html"
+		email.Msg = "使用Golang发送邮件"
+		email.Subject = "使用Golang发送邮件"
+		fmt.Println(email)
+		fmt.Println("Start Send msg", time.Now().Unix())
+
+		emailerr := sendemail.SendMail(email) // send email
+		if emailerr != nil {
+			fmt.Println("send mail error!")
+			fmt.Println(err)
+		}else{
+			fmt.Println("send mail success!")
+		}
+
+		erro := sendmsg.Sendmsg(msg, s) // send phone messages
+		if erro != nil {
+			fmt.Println(erro.Error())
+		}
+		fmt.Println("End Send msg", time.Now().Unix())
+
+		log.Println("发送成功！！")
+
+		//fmt.Fprintf(w, string(body))
+
+		fmt.Println("2. Item", item.Get("msg"))
+	}
 }
--- wxserver.go
+++ wxserver.go
@@ -4,15 +4,25 @@ import (
 	"log"
 	"net/http"
 	"./api"
+	//"./email"
 
+	"net/url"
+	"runtime"
 )
 
 
 //主函数
 func main() {
 	log.Println("微信服务: 启动!")
+	//email.SendEmail("明天出去游玩，各位带好东西")
+
+	passer := &api.DataPasser{FormData: make(chan url.Values)}
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	go passer.Log()
+
 	http.HandleFunc("/api/", api.ApiRequest)
 	http.HandleFunc("/wx/", api.Wxsend)
+	http.HandleFunc("/send/", passer.MakeRender)
 	err := http.ListenAndServe(":9001", nil)
 	if err != nil {
 		log.Fatal("微信服务: 端口监听和服务失败！, ", err)
